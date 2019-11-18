bd617e9cb36ebb69647848f8381c5551f7d54100
jcamposforner <jcamposforner@gmail.com>
2019-11-18 19:08:48 +0100

    First commit

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/foo', function(Request $request) {
-    return "Hello WOrld";
-});
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
